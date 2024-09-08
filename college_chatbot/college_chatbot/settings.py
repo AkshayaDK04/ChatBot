30d1c69ecbf12931f23c0e86c9ba8c32417e9987
AkshayaDK007 <akshayadk@student.tce.edu>
2024-09-08 06:40:21 +0530

    Little changes

--- college_chatbot/college_chatbot/settings.py
+++ college_chatbot/college_chatbot/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,8 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
